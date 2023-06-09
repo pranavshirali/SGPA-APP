2ecf8bd457cc39cd6917df0c30a04f84fc198d94
pranavshirali <pranavshirali007@gmail.com>
2023-06-09 16:36:23 +0530

    Changed codes here

--- app/src/main/java/com/appleassociates/sgpa/OptionScreen.java
+++ app/src/main/java/com/appleassociates/sgpa/OptionScreen.java
@@ -35,10 +35,8 @@ public class OptionScreen extends AppCompatActivity {
             boolean b = option2.equals("Computer Science") || option2.equals("AIML") || option2.equals("IOT") || option2.equals("Information Science") ||
                     option2.equals("Aeronautical") || option2.equals("Civil") || option2.equals("Electronics and Communication") ||
                     option2.equals("Mechatronics") || option2.equals("Mechanical");
-
-            //FOR 2021 SCHEME ALL BRANCHES
             if (option1.equals("2021 Scheme")){
-                if(b && option3.equals("SEM 3")){
+                        if(b && option3.equals("SEM 3")){
                     Intent intent = new Intent(OptionScreen.this, activity_3eng21.class);
                     startActivity(intent);
                 } else if (b && option3.equals("SEM 4")) {
@@ -56,15 +54,11 @@ public class OptionScreen extends AppCompatActivity {
                 } else if (b && option3.equals("SEM 8")) {
                     Intent intent = new Intent(OptionScreen.this, activity_8eng21.class);
                     startActivity(intent);
-                } else {
-                    // Display a message indicating that the selected options are not valid
-                    Toast.makeText(OptionScreen.this, "Selected option not available", Toast.LENGTH_SHORT).show();
                 }
             }
 
-            //FOR 2018 SCHEME ALL BRANCHES
             if (option1.equals("2018 Scheme")){
-                if(b && option3.equals("SEM 5")){
+                        if(b && option3.equals("SEM 5")){
                     Intent intent = new Intent(OptionScreen.this, activity_5eng18.class);
                     intent.putExtra("Aeronautical", option2);
                     startActivity(intent);
@@ -77,13 +71,17 @@ public class OptionScreen extends AppCompatActivity {
                 } else if (b && option3.equals("SEM 8")) {
                     Intent intent = new Intent(OptionScreen.this, activity_8eng18.class);
                     startActivity(intent);
-                } else {
-                    // Display a message indicating that the selected options are not valid
-                    Toast.makeText(OptionScreen.this, "Selected option not available", Toast.LENGTH_SHORT).show();
                 }
+            }
+
+
+
+
+
+            else {
+                // Display a message indicating that the selected options are not valid
+                Toast.makeText(OptionScreen.this, "Please select the correct options", Toast.LENGTH_SHORT).show();
             }
         });
     }
 }
-
-
